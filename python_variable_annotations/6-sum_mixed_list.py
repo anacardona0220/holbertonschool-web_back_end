2aa4eb034f6d85caa06fa81cd7fc611ad6292ba6
anacardona0220 <anamariacb0220@gmail.com>
2023-08-29 13:32:45 -0500

    Complex types - string and int/float to tuple

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import List, Union
 '''sum_mixed_list function'''
 
 
-def sum_mixed_list(mxd_lst: List[int][float]) -> float:
+def sum_mixed_list(mxd_lst: List [Union[int, float]]) -> float:
     '''retur sum'''
     return sum(mxd_lst)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import List
+from typing import Union, tuple
 '''to_kv function'''
 
 
-def to_kv(k: str, v: int ) -> float:
-    '''retur sum'''
-    return sum(mxd_lst)
+def to_kv(k: str, v: Union[int, float] ) -> tuple[str, float]:
+    '''retur k and square v'''
+    return (k, v * v)
