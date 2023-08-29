08f43044b9f168f0f6c203b873a6848e66c2c975
anacardona0220 <anamariacb0220@gmail.com>
2023-08-29 13:17:24 -0500

    Complex types - list of floats  Complex types - mixed list

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+'''sum_list function'''
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''retur sum'''
+    return sum(input_list)
---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import List
+'''sum_mixed_list function'''
+
+
+def sum_mixed_list(mxd_lst: List[int][float]) -> float:
+    '''retur sum'''
+    return sum(mxd_lst)
