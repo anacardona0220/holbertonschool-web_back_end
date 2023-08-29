b9a43f2ab77973d9174a9318d840e04471fdc9ae
anacardona0220 <anamariacb0220@gmail.com>
2023-08-29 15:28:56 -0500

    Let's duck type an iterable object

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-'''str, int, float for a tuple'''
+'''import callable'''
 from typing import Callable
 '''make_multiplier function'''
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    '''The first element of the tuple is the string k. The second
-    element is the square of the int/ float v and should
-    be annotated as a float'''
+    '''Write a type-annotated function make_multiplier that takes
+    a float multiplier as argument and returns a function that
+    multiplies a float by multiplier.'''
     def call(n: float):
         return n * multiplier
     return call
