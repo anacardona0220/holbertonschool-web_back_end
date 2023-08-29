9b0d61689b24afe3cc9d41e2e7b9de024ee243f2
anacardona0220 <anamariacb0220@gmail.com>
2023-08-29 15:32:25 -0500

    Let's duck type an iterable object

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+'''import list'''
 from typing import List, Union
 '''sum_mixed_list function'''
 
 
-def sum_mixed_list(mxd_lst: List [Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''retur sum'''
     return sum(mxd_lst)
