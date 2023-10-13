968ca5ebd5ea6a6f660f0e934bb2f2670b60c15b
anacardona0220 <anamariacb0220@gmail.com>
2023-10-13 17:29:07 -0500

    update file

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,6 +1,13 @@
-export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName,
-    lastName,
-  });
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  let msg = '';
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      msg += `${data[0].body} ${data[1].firstName} ${data[1].lastName}`;
+      console.log(msg);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
